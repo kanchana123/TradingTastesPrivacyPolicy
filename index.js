bb9f53584cfeae33e3f85fbfdb72add23904cab3
Kanchan Nannavare <kanchannannavare@Apples-MacBook-Pro.local>
2019-12-11 18:18:16 +0530

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+//create a server object:
+http.createServer(function (req, res) {
+  fs.readFile('privacyPolicy.html', function(err, data) {
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   res.write(data);
+   res.end();
+ });
+  // res.write('Hello World!'); //write a response to the client
+  // res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
